e4fdc37f347f20b63ea5065881409b4fb71e7103
Matt <53595224+davies3matt@users.noreply.github.com>
2020-09-27 20:19:23 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import nav from 'react-bootstrap/Nav';
 import { AnimatePresence, motion } from 'framer-motion';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee, faRocket, faPizzaSlice, faBook } from '@fortawesome/free-solid-svg-icons';
+import { faCheckSquare, faCoffee, faRocket, faPallet, faBook, faPalette, faFistRaised, faClipboardList,faComments,faClock, faBinoculars} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import $ from 'jquery'; 
 import interact from 'interactjs';
-library.add(fab, faCheckSquare, faCoffee, faGithub,faRocket,faBook);
+library.add(fab, faCheckSquare, faCoffee, faGithub,faRocket,faBook, faPallet, faFistRaised, faClipboardList,faComments, faClock);
 
 
 
@@ -23,9 +23,8 @@ function App() {
   return (
     
     <>
-      
     <div className="container-fluid bg-light" style={{ height: "100vh" }}>
-      <div className="row">
+      <div className="row gradient-bg">
         <nav
           className="col-6 col-sm-4 col-md-2 bg-dark py-5"
           style={{ height: "100vh", zIndex: 1000 }}
@@ -54,7 +53,7 @@ function App() {
         >
           <AnimatePresence>
             <Switch location={location} key={location.pathname}>
-              <Route path="/about" component={About} />
+              <Route path="/about" component={NewAbout} />
               <Route path="/settings" component={Settings} />
               <Route path="/" component={Home} />
             </Switch>
@@ -92,12 +91,16 @@ const pageTransition = {
 
 const pageStyle = {
   position: "absolute",
-   'width' : "1500px",
-  'margin-top' : "80px",
-  'margin-left' : "150px"
+   'width' : "1600px",
 };
 
-function About() {
+function NewAbout() {
+  const progressAngular = 45;
+  const progressReact = 50;
+  const progressHtml = 70;
+  const progressCss = 60;
+  const progressJavas = 65;
+
   return (
     <motion.div
       style={pageStyle}
@@ -106,40 +109,156 @@ function About() {
       exit="out"
       variants={pageVariants}
       transition={pageTransition}
-    >
-     <div id ="wrapper">
-        <div className="page">
-              <div className="container">
-                  <h1 className="text-light" style={{fontSize: '70px'}}>About</h1>
-              </div>  
-            <div id="blipWrapper">
-              <div id="hexagon" className="tap-hexagon">
-                <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
-              </div> 
-              <div id="hexagon" className="tap-hexagon">
-              </div>
-              <div id="hexagon" className="tap-hexagon">
-              </div>
-              </div>
-              <br></br>
-              <div id="blipWrapper">
-              <div id="hexagon" className="tap-hexagon">
-              </div>
-              <div id="hexagon" className="tap-hexagon">
-              </div>
-              <div id="hexagon" className="tap-hexagon">
-              </div>
-              </div>
+      >
+      <div className="container-fluid">
+        <div className="container">
+          <div className="row">
+           <div className="col-md-5">
+            <img className="img-circle" src="https://i.imgur.com/4f7ApkG.png"></img>
+	         </div>
+		       <div className="col-md-6">
+             <div className="row">
+               <h1 className="text-light center" style={{fontSize: '70px', padding: '20px'}}>Matthew Davies</h1>
+             </div>
+             <br></br>
+             <br></br>
+             <div className="row">
+               <p className="center text-light" style={{fontSize: '20px'}}> Hello there! I am an aspiring software developer and musician well on my way to receiving my Bsc IT degree by the
+                  end of 2020. Although primarily a back-end programmer I have a passion and love for fluid UI design and focusing on the user experience of applications.   </p>
+             </div>
+		       </div>
+          </div>
         </div>
-        <div id="statsWrapper">
-              <h1>Test</h1>
-              <div id="hexagon" className="tap-hexagon">
-                <h1>CSS</h1>
-              </div>
+        <br></br>
+        <br></br>
+        <div className="row">
+        <h1 className="text-light center" style={{fontSize: '55px', padding: '20px'}}>About</h1>
+        </div>
+        <div className="row space">
+          <div className="col-md-4">
+            <div id="hexagon" className="tap-hexagon center">
+             <FontAwesomeIcon icon={faBook} size="3x"></FontAwesomeIcon>
+            </div> 
+            <h2 className="h-skill text-light">Fast Learner</h2>
+            <p className="text-light center">I am a strategic thinker and a great listener.</p>
+          </div>
+          <div className="col-md-4">
+            <div id="hexagon" className="tap-hexagon center">
+              <FontAwesomeIcon icon={faPalette} size="3x"></FontAwesomeIcon>
             </div>
+            <h2 className="h-skill text-light">Creative</h2>
+            <p className="text-light center">My projects always include inspiring flares and inventive layouts.</p>
+          </div>
+          <div className="col-md-4">
+          <div id="hexagon" className="tap-hexagon center">
+              <FontAwesomeIcon icon={faFistRaised} size="3x"></FontAwesomeIcon>
+            </div>
+            <h2 className="h-skill text-light">Motivated</h2>
+            <p className="text-light center">I am young and road ready.</p>
+          </div>
         </div>
+        <br></br>
+        <hr></hr>
+        <div className="row">
+          <div className="col-md-4">
+            <div id="hexagon" className="tap-hexagon center">
+             <FontAwesomeIcon icon={faClipboardList} size="3x"></FontAwesomeIcon>
+            </div> 
+            <h2 className="h-skill text-light">Organized</h2>
+            <p className="text-light center">I keep my code neat, legible and logically sorted.</p>
+          </div>
+          <div className="col-md-4">
+            <div id="hexagon" className="tap-hexagon center">
+              <FontAwesomeIcon icon={faComments} size="3x"></FontAwesomeIcon>
+            </div>
+            <h2 className="h-skill text-light">Communication</h2>
+            <p className="text-light center">I have excellent people skills and love working with others.</p>
+          </div>
+          <div className="col-md-4">
+          <div id="hexagon" className="tap-hexagon center">
+              <FontAwesomeIcon icon={faClock} size="3x"></FontAwesomeIcon>
+            </div>
+            <h2 className="h-skill text-light">Time Management</h2>
+            <p className="text-light center">I like to work smart and plan ahead.</p>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="row">
+        <h1 className="text-light center" style={{fontSize: '55px', padding: '20px'}}>Skills</h1>
+        </div>
+        <div className="row space">
+        <div className="col-md-4">
+          <div className="container center vertical-center" style={{width: '70%', margin: 'auto'}}>
+            <h2 className="center text-light">These percentages are based on the knowledge that I have aquired during my degree</h2>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <h2 className="text-light center">Angular</h2>
+          <div className="progressbar-container">
+            <div className="progressbar-complete" style={{width: `${progressAngular}%`}}>
+              <div className="progressbar-liquid"></div>
+            </div>
+            <span className="progress"></span>
+          </div>
+          <h2 className="text-light center">React</h2>
+          <div className="progressbar-container">
+            <div className="progressbar-complete" style={{width: `${progressReact}%`}}>
+              <div className="progressbar-liquid"></div>
+            </div>
+            <span className="progress"></span>
+          </div>    
+          <h2 className="text-light center">HTML</h2>
+          <div className="progressbar-container">
+            <div className="progressbar-complete" style={{width: `${progressHtml}%`}}>
+              <div className="progressbar-liquid"></div>
+            </div>
+            <span className="progress"></span>
+          </div>  
+          <h2 className="text-light center">CSS</h2>
+          <div className="progressbar-container">
+            <div className="progressbar-complete" style={{width: `${progressCss}%`}}>
+              <div className="progressbar-liquid"></div>
+            </div>
+            <span className="progress"></span>
+          </div>  
+          <h2 className="text-light center">JavaScript</h2>
+          <div className="progressbar-container">
+            <div className="progressbar-complete" style={{width: `${progressJavas}%`}}>
+              <div className="progressbar-liquid"></div>
+            </div>
+            <span className="progress"></span>
+          </div>  
+        </div>
+        <div className="col-md-4">  
+        </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <h1 className="text-light center space" style={{fontSize: '55px', padding: '20px'}}>Education</h1>
+        </div>
+        <div className="row space">
+          <div className="col-md-6 center">
+            <div className="container center">
+              <h2 className="text-light center" style={{paddingTop: '30px'}}>BACHELOR OF SCIENCE, MAJOR IN INFORMATION TECHNOLOGY</h2>
+              <br></br>
+              <img className="center" src="https://i.imgur.com/mTl82x8.png" style={{width: '400px', height: 'auto'}}></img>
+              <div className="btn-group mt-2 mb-4" role="group" aria-label="actionButtons">
+                <a href="http://www.nwu.ac.za/" target="_blank" className="d-block btn btn-outline-light" download><i className="fas fa-file-download mr-2" /><FontAwesomeIcon icon={faRocket} /> Visit NWU</a>
+               <a href="http://www.nwu.ac.za/sites/www.nwu.ac.za/files/files/p-csis/documents/BSc_in_IT.pdf" target="_blank" className="d-block btn btn-outline-light"> <FontAwesomeIcon icon={faBinoculars} /> View Degree Summary<i className="fas fa-external-link-square-alt ml-2" /></a>
+             </div>
+            </div>
+            <br></br>
+          </div>
+          <div className="col-md-6">
+
+          </div>
+       </div>
+        <br></br>
+        <br></br>
+      </div>
     </motion.div>
-  );
+  )
 }
 
   interact('.tap-hexagon')
@@ -245,16 +364,15 @@ function Home() {
       variants={pageVariants}
       transition={pageTransition}
     >
-      <div id ="wrapper">
-        <div id="statsWrapper"></div>
-        <div className="page">
-              <div className="container">
-                  <h1 className="text-light" style={{fontSize: '70px'}}>Matthew Davies</h1>
-                  <div className="btn-group mt-2 mb-4" role="group" aria-label="actionButtons">
-                      <button onClick={activateShow} className="d-block btn btn-outline-light" download><i className="fas fa-file-download mr-2" /><FontAwesomeIcon icon={faRocket} /> Start Tour</button>
-                      <button href="https://github.com/davies3matt" target="_blank" className="d-block btn btn-outline-light"> <FontAwesomeIcon icon={faGithub} /> Visit My Github Profile<i className="fas fa-external-link-square-alt ml-2" /></button>
-                  </div>
-              </div>  
+       <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12 center">
+           <h1 className="text-light center" style={{fontSize: '70px'}}>Matthew Davies</h1>
+           <div className="btn-group mt-2 mb-4" role="group" aria-label="actionButtons">
+            <a className="d-block btn btn-outline-light" download><i className="fas fa-file-download mr-2" /><FontAwesomeIcon icon={faRocket} /> Start Tour</a>
+            <a href="https://github.com/davies3matt" target="https://github.com/davies3matt" className="d-block btn btn-outline-light"> <FontAwesomeIcon icon={faGithub} /> Visit My Github Profile<i className="fas fa-external-link-square-alt ml-2" /></a>
+           </div>
+          </div>
         </div>
       </div>
     </motion.div>
